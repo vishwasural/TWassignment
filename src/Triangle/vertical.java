90337fa09e20d286a473d2893ec877e59d009654
vishwasural <vishwasural@gmail.com>
2017-12-10 01:45:20 +0530

    Vertical

---
+++ src/Triangle/vertical.java
@@ -0,0 +1,14 @@
+package Triangle;
+
+import java.util.Scanner;
+
+public class vertical {
+    public static void main(String args[]) {
+        int n;
+        Scanner ipScnr = new Scanner(System.in);
+        n = ipScnr.nextInt();
+        for(int i=0; i<n; i++) {
+            System.out.println("*");
+        }
+    }
+}
