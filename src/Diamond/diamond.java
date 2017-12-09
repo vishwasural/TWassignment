c4aac7b6f118523d8eaacd1c0a9ed1bbc60fa990
vishwasural <vishwasural@gmail.com>
2017-12-10 02:22:44 +0530

    diamond

---
+++ src/Diamond/diamond.java
@@ -0,0 +1,43 @@
+package Diamond;
+
+import java.util.Scanner;
+
+public class diamond {
+    public static void main(String args[]) {
+        int n;
+        Scanner ipScnr = new Scanner(System.in);
+        n = ipScnr.nextInt();
+        int gap = n-1;
+        for(int i=0; i<n; i++) {
+            for(int j=0;j<gap;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++) {
+                System.out.print("*");
+            }
+            for(int j=0;j<gap;j++)
+            {
+                System.out.print(" ");
+            }
+            gap--;
+            System.out.println();
+        }
+        gap++;
+        for(int i=n-2; i>=0; i--) {
+            for(int j=0;j<=gap;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++) {
+                System.out.print("*");
+            }
+            for(int j=0;j<=gap;j++)
+            {
+                System.out.print(" ");
+            }
+            gap++;
+            System.out.println();
+        }
+    }
+}
